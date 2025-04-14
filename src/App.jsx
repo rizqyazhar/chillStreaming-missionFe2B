9773e7d5902a7f2fb64abaad08edfe739b2a3723
rizqyazhar <rizqytask20@gmail.com>
2025-04-14 13:14:29 +0700

    add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 
+const router = createBrowserRouter([{ path: "/", element: <LoginPage /> }]);
+
 const App = () => {
-  return <LoginPage />;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
