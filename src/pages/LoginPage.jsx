7eaecea9394de07c3b0e38cf22c85278f6d2997f
rizqyazhar <rizqytask20@gmail.com>
2025-04-14 13:33:29 +0700

    feat: registerpage, add reac-icon in loginpage and registerpage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import Button from "../atoms/Button";
 import AuthLayout from "../layouts/AuthLayout";
 import ButtonFields from "../molecules/ButtonFields";
 import InputFields from "../molecules/InputFields";
+import { BiSolidHide } from "react-icons/bi";
 
 const LoginPage = () => {
   return (
@@ -19,6 +20,7 @@ const LoginPage = () => {
         placeholder='Masukkan kata sandi'
         htmlFor='password'
         labelText='Kata Sandi'>
+        <BiSolidHide className='absolute top-[29px] md:top-[50px] right-3 md:right-5 text-xs md:text-2xl text-light-disabled cursor-pointer' />
         <div className='w-full flex justify-between font-lato mt-1.5'>
           <p className='text-light-secondary text-[10px] md:text-base tracking-[.2px] cursor-default'>
             Belum punya akun?{" "}
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -2,6 +2,7 @@ import Button from "../atoms/Button";
 import AuthLayout from "../layouts/AuthLayout";
 import ButtonFields from "../molecules/ButtonFields";
 import InputFields from "../molecules/InputFields";
+import { BiSolidHide } from "react-icons/bi";
 
 const LoginPage = () => {
   return (
@@ -18,14 +19,16 @@ const LoginPage = () => {
         type='password'
         placeholder='Masukkan kata sandi'
         htmlFor='password'
-        labelText='Kata Sandi'
-      />
+        labelText='Kata Sandi'>
+        <BiSolidHide className='absolute top-7 md:top-[50px] right-3 md:right-5 text-xs md:text-2xl text-light-disabled cursor-pointer' />
+      </InputFields>
       <InputFields
-        id='password'
+        id='confirm-password'
         type='password'
         placeholder='Masukkan kata sandi'
         htmlFor='password'
         labelText='Konfirmasi Kata Sandi'>
+        <BiSolidHide className='absolute top-[29px] md:top-[50px] right-3 md:right-5 text-xs md:text-2xl text-light-disabled cursor-pointer' />
         <div className='w-full flex justify-between font-lato mt-1.5'>
           <p className='text-light-secondary text-[10px] md:text-base tracking-[.2px] cursor-default'>
             Belum punya akun?{" "}
