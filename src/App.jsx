77ab0ccdd31f953dc2d40d31eeef9247c9a30889
rizqyazhar <rizqytask20@gmail.com>
2025-04-15 17:45:17 +0700

    feat: seriespage done

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import FilmsPage from "./pages/FilmsPage";
+import SeriesPage from "./pages/SeriesPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <LoginPage /> },
   { path: "/register", element: <RegisterPage /> },
   { path: "/home", element: <HomePage /> },
   { path: "/films", element: <FilmsPage /> },
+  { path: "/series", element: <SeriesPage /> },
 ]);
 
 const App = () => {
---
+++ src/pages/SeriesPage.jsx
@@ -0,0 +1,26 @@
+import HomeLayout from "../layouts/HomeLayout";
+import Hero from "../fragments/Hero";
+import Content from "../fragments/Content";
+import PotraitCardBox from "../fragments/PotraitCardBox";
+import LandscapeCardBox from "../fragments/LandscapeCardBox";
+import Genre from "../fragments/genre/Genre";
+import GenreTab from "../fragments/genre/GenreTab";
+
+const FilmsPage = () => {
+  return (
+    <HomeLayout genreTab={<GenreTab width='100px' />}>
+      <Hero>
+        <Genre width='392px' />
+      </Hero>
+      <Content>
+        <LandscapeCardBox title='Melanjutkan Tonton Series' />
+        <PotraitCardBox title='Series Persembahan Chill' />
+        <PotraitCardBox title='Top Rating Series Hari ini' />
+        <PotraitCardBox title='Series Trending' />
+        <PotraitCardBox title='Rilis Baru' />
+      </Content>
+    </HomeLayout>
+  );
+};
+
+export default FilmsPage;
