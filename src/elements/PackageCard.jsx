de24ba7b58206677a0d3ea0637c2d6f664e04273
rizqyazhar <rizqytask20@gmail.com>
2025-04-17 20:59:56 +0700

    feat: membershippage, change to package car component

---
+++ src/elements/PackageCard.jsx
@@ -0,0 +1,43 @@
+const PackageCard = ({
+  packageTitle,
+  packagePrice,
+  packageAmountAccount,
+  packageQualityDesc,
+}) => {
+  return (
+    <div className='flex flex-col gap-6 p-6 rounded-xl bg-linear-to-r from-[#537cd4]/100 from-1% to-primary-100 to-99%'>
+      <h5 className='w-fit rounded-full bg-extraBackground px-5 py-2.5 font-semibold text-base'>
+        {packageTitle}
+      </h5>
+      <div className='flex flex-col gap-1 text-xs font-normal'>
+        <p>Mulai dari Rp{packagePrice}/bulan</p>
+        <p>{packageAmountAccount} Akun</p>
+      </div>
+      <div className='flex flex-col gap-2 font-normal text-sm'>
+        <p className='flex gap-2 items-center'>
+          <img src='/public/img/icon/check.svg' className='w-5' />
+          Tidak ada iklan
+        </p>
+        <p className='flex gap-2 items-center'>
+          <img src='/public/img/icon/check.svg' className='w-5' />
+          Kualitas {packageQualityDesc}
+        </p>
+        <p className='flex gap-2 items-center'>
+          <img src='/public/img/icon/check.svg' className='w-5' />
+          Download konten pilihan
+        </p>
+      </div>
+      <div className='border border-outlineBorder'></div>
+      <div className='flex flex-col items-center gap-1'>
+        <button className='w-full flex items-center rounded-full justify-center px-[26px] py-2.5 bg-light-primary text-primary-300 font-bold text-sm cursor-pointer'>
+          Langganan
+        </button>
+        <p className='text-light-primary font-normal text-xs'>
+          Syarat dan Ketentuan Berlaku
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PackageCard;
--- src/fragments/membership/PackagesMembership.jsx
+++ src/fragments/membership/PackagesMembership.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PackageCard from "../../elements/PackageCard";
 
 const PackagesMembership = () => {
   return (
@@ -10,102 +11,24 @@ const PackagesMembership = () => {
         </p>
       </div>
       <div className='flex flex-col md:flex-row gap-8 md:gap-[72px]'>
-        <div className='flex flex-col gap-6 p-6 rounded-xl bg-linear-to-r from-[#537cd4]/100 from-1% to-primary-100 to-99%'>
-          <h5 className='w-fit rounded-full bg-extraBackground px-5 py-2.5 font-semibold text-base'>
-            Individual
-          </h5>
-          <div className='flex flex-col gap-1 text-xs font-normal'>
-            <p>Mulai dari Rp49,990/bulan</p>
-            <p>1 Akun</p>
-          </div>
-          <div className='flex flex-col gap-2 font-normal text-sm'>
-            <p className='flex gap-2 items-center'>
-              <img src='/public/img/icon/check.svg' className='w-5' />
-              Tidak ada iklan
-            </p>
-            <p className='flex gap-2 items-center'>
-              <img src='/public/img/icon/check.svg' className='w-5' />
-              Kualitas 720p
-            </p>
-            <p className='flex gap-2 items-center'>
-              <img src='/public/img/icon/check.svg' className='w-5' />
-              Download konten pilihan
-            </p>
-          </div>
-          <div className='border border-outlineBorder'></div>
-          <div className='flex flex-col items-center gap-1'>
-            <button className='w-full flex items-center rounded-full justify-center px-[26px] py-2.5 bg-light-primary text-primary-300 font-bold text-sm cursor-pointer'>
-              Langganan
-            </button>
-            <p className='text-light-primary font-normal text-xs'>
-              Syarat dan Ketentuan Berlaku
-            </p>
-          </div>
-        </div>
-        <div className='flex flex-col gap-6 p-6 rounded-xl bg-linear-to-r from-[#537cd4]/100 from-1% to-primary-100 to-99%'>
-          <h5 className='w-fit rounded-full bg-extraBackground px-5 py-2.5 font-semibold text-base'>
-            Berdua
-          </h5>
-          <div className='flex flex-col gap-1 text-xs font-normal'>
-            <p>Mulai dari Rp79,990/bulan</p>
-            <p>2 Akun</p>
-          </div>
-          <div className='flex flex-col gap-2 font-normal text-sm'>
-            <p className='flex gap-2 items-center'>
-              <img src='/public/img/icon/check.svg' className='w-5' />
-              Tidak ada iklan
-            </p>
-            <p className='flex gap-2 items-center'>
-              <img src='/public/img/icon/check.svg' className='w-5' />
-              Kualitas 1080p
-            </p>
-            <p className='flex gap-2 items-center'>
-              <img src='/public/img/icon/check.svg' className='w-5' />
-              Download konten pilihan
-            </p>
-          </div>
-          <div className='border border-outlineBorder'></div>
-          <div className='flex flex-col items-center gap-1'>
-            <button className='w-full flex items-center rounded-full justify-center px-[26px] py-2.5 bg-light-primary text-primary-300 font-bold text-sm cursor-pointer'>
-              Langganan
-            </button>
-            <p className='text-light-primary font-normal text-xs'>
-              Syarat dan Ketentuan Berlaku
-            </p>
-          </div>
-        </div>
-        <div className='flex flex-col gap-6 p-6 rounded-xl bg-linear-to-r from-[#537cd4]/100 from-1% to-primary-100 to-99%'>
-          <h5 className='w-fit rounded-full bg-extraBackground px-5 py-2.5 font-semibold text-base'>
-            Keluarga
-          </h5>
-          <div className='flex flex-col gap-1 text-xs font-normal'>
-            <p>Mulai dari Rp159,990/bulan</p>
-            <p>5-7 Akun</p>
-          </div>
-          <div className='flex flex-col gap-2 font-normal text-sm'>
-            <p className='flex gap-2 items-center'>
-              <img src='/public/img/icon/check.svg' className='w-5' />
-              Tidak ada iklan
-            </p>
-            <p className='flex gap-2 items-center'>
-              <img src='/public/img/icon/check.svg' className='w-5' />
-              Kualitas 4K
-            </p>
-            <p className='flex gap-2 items-center'>
-              <img src='/public/img/icon/check.svg' className='w-5' />
-              Download konten pilihan
-            </p>
-          </div>
-          <div className='border border-outlineBorder'></div>
-          <div className='flex flex-col items-center gap-1'>
-            <button className='w-full flex items-center rounded-full justify-center px-[26px] py-2.5 bg-light-primary text-primary-300 font-bold text-sm cursor-pointer'>
-              Langganan
-            </button>
-            <p className='text-light-primary font-normal text-xs'>
-              Syarat dan Ketentuan Berlaku
-            </p>
-          </div>
-        </div>
+        <PackageCard
+          packageTitle='Individual'
+          packagePrice='49,990'
+          packageAmountAccount='1'
+          packageQualityDesc='720p'
+        />
+        <PackageCard
+          packageTitle='Berdua'
+          packagePrice='79,990'
+          packageAmountAccount='2'
+          packageQualityDesc='1080p'
+        />
+        <PackageCard
+          packageTitle='Keluarga'
+          packagePrice='159,990'
+          packageAmountAccount='5-7'
+          packageQualityDesc='4K'
+        />
       </div>
     </section>
   );
